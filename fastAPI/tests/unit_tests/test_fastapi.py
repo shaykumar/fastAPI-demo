fd2c224787f0594fb736de71b103a87d88bbd965
EC2 Default User <ec2-user@ip-172-31-33-216.ap-southeast-2.compute.internal>
2022-05-31 09:08:38 +0000

    Adding test cases and structure

---
+++ fastAPI/tests/unit_tests/test_fastapi.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+"""
+Test for the main page using fastapi.testclient.
+"""
+import os
+import sys
+from fastapi.testclient import TestClient
+
+print("In module products sys.path[0], __package__ ==", sys.path[0], __package__)
+
+sys.path.append("/home/ec2-user/fastAPI/")
+
+print(sys.path)
+
+from main import app
+
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ 
+# Declaring test client
+client = TestClient(app)
+  
+# Test for status code for get message
+def test_get_home_url(): 
+  response = client.get("/")
+  assert response.status_code == 200
+                   
